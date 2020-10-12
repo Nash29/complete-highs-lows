bc4e245223a9e1900054036f2cec1c298c979fc7
murilo <nmurilo164@gmail.com>
2020-10-12 14:33:20 -0300

    Death Valley

--- complete_highs_lows.py
+++ complete_highs_lows.py
@@ -24,19 +24,16 @@ with open(filename) as f:
 	# Obtém as datas e as temperaturas maximas e minimas do arquivo
 	dates, highs, lows = [], [], []
 	for row in reader:
-		# converte as datas para um datetime
-		current_date = datetime.strptime(row[0], '%Y-%m-%d')
-		dates.append(current_date)
-		
-		
-		# converte a temperatura maxima para inteiro
-		high = int(row[1])
-		highs.append(high)
-		
-		
-		# converte a temperatura minima para inteiro
-		low = int(row[3])
-		lows.append(low)
+		try:
+			current_date = datetime.strptime(row[0], '%Y-%m-%d')
+			high = int(row[1])
+			low = int(row[3])
+		except ValueError:
+			print(current_date, 'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
 		
 	print(highs)
 	
@@ -50,7 +47,8 @@ plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 
 # Formata o grafico
-plt.title('Daily high and low temperatures - 2014', fontsize=24)
+title = 'Daily high and low temperatures - 2014\nDeath Valley, CA'
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 plt.ylabel('Temperature (F)', fontsize=16)
 fig.autofmt_xdate()# desenha as datas em diagonal
